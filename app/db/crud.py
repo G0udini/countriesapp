7e569f362be9e72ab068666f452e9d00814a259a
G0udini <ruslan.shabunin2010@yandex.ru>
2022-02-03 23:07:18 +0300

    add crud for cities

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,9 +1,49 @@
 from .base import AsyncIOMotorClient
 from ..core.config import CITY_COLLECTION
+from .schemas import FullCity, ViewCity
 
+import pymongo
 from slugify import slugify
 from bson import ObjectId
 
 
-async def get_all_cities(conn: AsyncIOMotorClient) -> list:
-    return await conn[CITY_COLLECTION].find().to_list(length=100)
+async def get_all_cities(
+    conn: AsyncIOMotorClient, limit: int, skip: int
+) -> list[ViewCity]:
+    return await conn[CITY_COLLECTION].find(skip=skip).to_list(length=limit)
+
+
+async def get_city_by_slug(conn: AsyncIOMotorClient, slug: str) -> ViewCity:
+    return await conn[CITY_COLLECTION].find_one({"slug": slug})
+
+
+async def get_cities_by_rating(
+    conn: AsyncIOMotorClient, limit: int, skip: int
+) -> list[ViewCity]:
+    return (
+        await conn[CITY_COLLECTION]
+        .find()
+        .sort("rating", pymongo.DESCENDING)
+        .skip(skip)
+        .to_list(length=limit)
+    )
+
+
+async def insert_city_and_return(conn: AsyncIOMotorClient, document: ViewCity):
+    city_doc = document.dict()
+    city_doc["slug"] = slugify(city_doc["name"])
+    await conn[CITY_COLLECTION].insert_one(city_doc)
+    return city_doc
+
+
+async def update_city_and_return(
+    conn: AsyncIOMotorClient, slug: str, document: ViewCity
+):
+    city_doc = document.dict()
+    city_doc["slug"] = slugify(city_doc["name"])
+    await conn[CITY_COLLECTION].update_one({"slug": slug}, {"$set": city_doc})
+    return city_doc
+
+
+async def delete_city_and_return(conn: AsyncIOMotorClient, slug: str):
+    await conn[CITY_COLLECTION].delete_one({"slug": slug})
--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI, Request
+from fastapi import Body, FastAPI, HTTPException, Path, Query, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 
 from .db.schemas import ViewCity, FullCity, InputSight, FullSight
-from .db.crud import get_all_cities
+from .db.crud import (
+    get_all_cities,
+    get_city_by_slug,
+    get_cities_by_rating,
+    insert_city_and_return,
+    update_city_and_return,
+    delete_city_and_return,
+)
 from .db.base import create_connection
 
 app = FastAPI()
@@ -30,9 +37,71 @@ async def shutdown_db_client():
     "/api/cities/",
     response_model=list[ViewCity],
     response_description="List all cities",
+    tags=["city"],
 )
-async def list_all_cities(request: Request):
-    return await get_all_cities(conn=app.mongodb)
+async def list_all_cities(limit: int = Query(20, gt=0), skip: int = Query(0, ge=0)):
+    return await get_all_cities(conn=app.mongodb, limit=limit, skip=skip)
+
+
+@app.post(
+    "/api/cities",
+    response_model=FullCity,
+    response_description="Add new city",
+    tags=["city"],
+)
+async def add_city(document: ViewCity = Body(...)):
+    return await insert_city_and_return(conn=app.mongodb, document=document)
+
+
+@app.get(
+    "/api/city/{slug}/",
+    response_model=ViewCity,
+    response_description="Get city",
+    tags=["city"],
+)
+async def get_city(slug: str = Path(..., min_length=1)):
+    city = await get_city_by_slug(conn=app.mongodb, slug=slug)
+    if city is not None:
+        return city
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"City {slug} not found"
+    )
+
+
+@app.put(
+    "/api/city/{slug}/",
+    response_model=ViewCity,
+    response_description="Update city",
+    tags=["city"],
+)
+async def update_city(
+    slug: str = Path(..., min_length=1), document: ViewCity = Body(...)
+):
+    city = await update_city_and_return(conn=app.mongodb, slug=slug, document=document)
+    if city is not None:
+        return city
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"City {slug} not found"
+    )
+
+
+@app.delete(
+    "/api/city/{slug}/",
+    response_description="Delete city",
+    tags=["city"],
+)
+async def delete_city(slug: str = Path(..., min_length=1)):
+    await delete_city_and_return(conn=app.mongodb, slug=slug)
+
+
+@app.get(
+    "/api/cities/top/",
+    response_model=list[ViewCity],
+    response_description="Get the most rated cities",
+    tags=["city"],
+)
+async def get_city_rating(limit: int = Query(20, gt=0), skip: int = Query(0, ge=0)):
+    return await get_cities_by_rating(conn=app.mongodb, limit=limit, skip=skip)
 
 
 # @app.get("/api/todo/{id}", response_model=ResponseTodo)
